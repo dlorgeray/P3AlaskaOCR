31bf53fa89dcbacf40073dfdb6e96998cefe8406
dlorgeray <damien.lorgeray@gmail.com>
2018-10-03 11:28:12 +0200

    Modification du gabarit Ajout des gabarits Header/Footer/Nav

---
+++ View/Template/HeaderTemplate.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <base href="<?= $rootWeb ?>">
+    <title><?= $title ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="Content/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="./Content/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet'
+          type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+          rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="./Content/css/clean-blog.min.css" rel="stylesheet">
+
+</head>
+<body>
+<?php include('View/Template/NavTemplate.php'); ?>
+
+<!-- Page Header -->
+<header class="masthead" style="background-image: url('./Content/img/home-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="site-heading">
+                    <h1>Billet Simple pour l'Alaska</h1>
+                    <span class="subheading">de Jean FORTEROCHE</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
