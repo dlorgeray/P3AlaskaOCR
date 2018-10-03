97eefe76491c3831b22dde54b631c677c6f0178e
dlorgeray <damien.lorgeray@gmail.com>
2018-10-03 11:39:19 +0200

    Modification des vues connexion et administration pour harmonisation avec le template

--- View/Admin/index.php
+++ View/Admin/index.php
@@ -1,7 +1,13 @@
 <?php $this->title = "Billet simple pour l'Alaska - Administration" ?>
-<h2>Administration</h2>
-Bienvenue, <?= $this->clean( $login ) ?> !
+
+<div class="post-preview" xmlns="http://www.w3.org/1999/html">
+
+    <h2>Administration</h2>
+    Bienvenue, <?= $this->clean( $login ) ?> ! </br>
 Ce blog comporte <?= $this->clean( $nbPosts ) ?> billet(s) et
 <?= $this->clean( $nbComments ) ?> commentaire(s).
 J.Forteroche
-<a id="lienDeco" href="connexion/deconnect">Se déconnecter</a>
+    <div class="form-group">
+        <a id="lienDeco" class="btn btn-primary" href="connexion/deconnect">Se déconnecter</a>
+    </div>
+</div>
