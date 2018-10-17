8f1f991085c5a2232f99644276c574495b8e0fba
dlorgeray <damien.lorgeray@gmail.com>
2018-10-17 14:34:44 +0200

    Ajout des gabarits du template admin

---
+++ View/adminTemplate.php
@@ -0,0 +1,17 @@
+<?php include('View/Template/AdminHeaderTemplate.php'); ?>
+
+<body class="nav-md">
+<div class="container body">
+    <div class="main_container">
+        <div class="col-md-3 left_col">
+            <div class="left_col scroll-view">
+
+                <?php include('View/Template/AdminNavTemplate.php'); ?>
+
+                <!-- content -->
+
+                <?= $content ?>
+
+
+                <?php include('View/Template/AdminFooterTemplate.php'); ?>
+
