efaf8c1d6183f24d930c727871beff8350d08dd4
dlorgeray <damien.lorgeray@gmail.com>
2018-10-17 14:44:57 +0200

    Ajout des controlleurs pour la gestion des chapitres et des commentaires

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -2,6 +2,7 @@
 require_once 'Controller/SecureController.php';
 require_once 'Model/Post.php';
 require_once 'Model/Comment.php';
+require_once 'Model/Report.php';
 
 /**
  * Contrôleur des actions d'administration
@@ -10,6 +11,7 @@ class AdminController extends SecureController
 {
     private $post;
     private $comment;
+    private $report;
 
     /**
      * Constructeur
@@ -18,23 +20,44 @@ class AdminController extends SecureController
     {
         $this->post = new Post();
         $this->comment = new Comment();
+        $this->report = new Report();
+        $this->setTemplate( View::ADMIN_LAYOUT );
+
     }
 
     public function index ()
     {
         $nbPosts = $this->post->getPostsNomber();
         $nbComments = $this->comment->getCommentsNomber();
+        $nbReports = $this->report->getReportsNumber();
+
         $posts = $this->post->getPosts();
         $login = $this->request->getSession()->getAttribut( "login" );
-        $this->generateView( array ( 'nbPosts' => $nbPosts ,
-            'nbComments' => $nbComments , 'posts' => $posts , 'login' => $login ) );
+        $this->generateView(
+            array ( 'nbPosts' => $nbPosts , 'nbComments' => $nbComments , 'nbReports' => $nbReports , 'posts' => $posts , 'login' => $login )
+        );
 
+    }
+
+    public function managePost ()
+    {
+        $nbPosts = $this->post->getPostsNomber();
+        $nbComments = $this->comment->getCommentsNomber();
+        /**
+         *$nbReports = $this->report->getReportsComment();
+         */
+        $posts = $this->post->getPosts();
+        $login = $this->request->getSession()->getAttribut( "login" );
+        $this->generateView(
+            array ( 'nbPosts' => $nbPosts , 'nbComments' => $nbComments , /**'nbReports' => $nbReports,*/
+                'posts' => $posts , 'login' => $login )
+        );
     }
 
     /**
      * Ecrire un nouveau chapitre du roman
      */
-    public function writing ()
+    public function writingPost ()
     {
         $title = $this->request->getSetting( "new_title" );
         if ($this->request->existSetting( "new_content" )) {
@@ -44,6 +67,11 @@ class AdminController extends SecureController
         }
         $this->post->create( $title , $content );
         $this->redirect( 'Admin' , 'index/' );
+    }
+
+    public function writePost ()
+    {
+        $this->generateView();
     }
 
     /**
@@ -79,6 +107,12 @@ class AdminController extends SecureController
     {
         $idPost = $this->request->getSetting( 'id' );
         $this->post->delete( $idPost );
-        $this->redirect( 'Admin' , 'index/' );
+        $this->redirect( 'Admin' , 'managePost/' . $idPost );
+    }
+
+    public function manageComment ()
+    {
+        $allComments = $this->comment->getAllComments();
+        $this->generateView( array ( 'allComments' => $allComments ) );
     }
 }
