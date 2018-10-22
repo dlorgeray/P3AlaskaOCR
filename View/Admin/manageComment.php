3a037cc75e245f68ee21d4736dfde0d342c7f4cb
dlorgeray <damien.lorgeray@gmail.com>
2018-10-22 14:28:57 +0200

    Modification de la vue

--- View/Admin/manageComment.php
+++ View/Admin/manageComment.php
@@ -10,21 +10,28 @@
                 <table class="table table-hover table-striped table-dark">
                     <thead>
                     <tr>
-                        <th scope="col">Auteur</th>
-                        <th scope="col">Date de création</th>
                         <th scope="col">Chapitre</th>
+                        <th scope="col">Date du commentaire</th>
+                        <th scope="col">Auteur</th>
                         <th scope="col">Commentaire</th>
+                        <th scope="col">Signalement</th>
                     </tr>
                     </thead>
                     <tbody>
                     <?php foreach ($allComments as $comment): ?>
                         <tr>
-                            <td><?= $this->clean( $comment['author'] ) ?></td>
+                            <td><?= $this->clean( $comment['id'] ) ?></td>
                             <td>
                                 <time><?= $this->clean( $comment['date'] ) ?></time>
                             </td>
-                            <td><?= $this->clean( $comment['id'] ) ?></td>
+                            <td><?= $this->clean( $comment['author'] ) ?></td>
                             <td><?= $this->clean( $comment['content'] ) ?></td>
+                            <td><?php if ($comment['idReport'] == null) {
+                                    echo $comment['idReport'] = 'NON';
+                                } else {
+                                    echo $comment['idReport'] = 'OUI';
+                                }
+                                ?></td>
                         </tr>
                     <?php endforeach; ?>
                     </tbody>
--- View/Post/lastPost.php
+++ View/Post/lastPost.php
@@ -28,3 +28,58 @@
         <?php endforeach; ?>
     </section>
 </article>
+
+<section class="wrapper style2">
+    <div class="inner">
+        <header id="liste" class="major">
+            <h3 id="titleReply">Commentaires des lecteurs sur le <?= $this->clean( $post['title'] ) ?></h3>
+        </header>
+
+        <?php foreach ($posts as $comment): ?>
+            <time><?= $this->clean( $comment['dateComment'] ) ?></time>
+            <p><?= $this->clean( $comment['authorComment'] ) ?> dit :</p>
+            <p><?= $this->clean( $comment['contentComment'] ) ?></p>
+            <div class="control-group">
+                <div class="form-group floating-label-form-group controls mb-0 pb-2"></div>
+                <form method="post" action="post/reportComment">
+                    <input type="hidden" name="idPost" value="<?= $post['id'] ?>"/>
+                    <button type="submit" name="idComment" value="<?= $this->clean( $comment['id'] ) ?>"
+                            class="button special">Signaler
+                    </button>
+                </form>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</section>
+
+<section class="wrapper style2">
+    <div class="inner">
+        <?php $this->getFlash() ?>
+        <h3>Ajouter un commentaire</h3>
+
+        <form action="post/comment" onSubmit="return actionlimite();" method="post" name="sentComment">
+            <div class="control-group">
+                <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                    <label>Pseudo</label>
+                    <input class="form-control" name="author" type="text" placeholder="Votre pseudo" value="">
+                    <p class="help-block text-danger"></p>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                    <label>Votre commentaire</label>
+                    <textarea class="form-control" name="content" rows="5"
+                              onKeyUp="limite(this);" placeholder="Votre commentaire" required></textarea>
+                    <p class="help-block text-danger"></p>
+                </div>
+            </div>
+            <br>
+            <div id="success"></div>
+            <input type="hidden" name="id" value="<?= $post['id'] ?>"/>
+            <div class="form-group">
+                <input type="submit" class="button special" value="Ajouter le commentaire"/>
+            </div>
+
+        </form>
+    </div>
+</section>
