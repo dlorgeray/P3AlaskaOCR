15bfd115046527f0fab653271f09b616d2849e9e
dlorgeray <damien.lorgeray@gmail.com>
2018-10-25 18:46:30 +0200

    Modification des chemins d'accès aux fichiers du template

--- View/Template/HeaderTemplate.php
+++ View/Template/HeaderTemplate.php
@@ -19,20 +19,20 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
           type="text/css">
     <!-- Custom Fonts -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="Content/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic"
           rel="stylesheet" type="text/css">
-    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+    <link href="Content/vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
 
     <!-- Plugin CSS -->
-    <link href="Content/vendor/magnific-popup.css/magnific-popup.css" rel="stylesheet" type="text/css">
+    <link href="Content/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
 
-    <script type="text/javascript" src="/Content/tinymce/tinymce.min.js"></script>
+    <script type="text/javascript" src="/P3AlaskaOCR/Content/tinymce/tinymce.min.js"></script>
 
     <!-- Custom styles for this template -->
-    <link href="./Content/css/freelancer.min.css" rel="stylesheet">
-    <link href="./Content/css/clean-blog.css" rel="stylesheet">
-    <link href="./Content/css/stylish-portfolio.css" rel="stylesheet">
+    <link href="Content/css/freelancer.min.css" rel="stylesheet">
+    <link href="Content/css/clean-blog.css" rel="stylesheet">
+    <link href="Content/css/stylish-portfolio.css" rel="stylesheet">
 </head>
 <body id="page-top">
 <?php include('View/Template/NavTemplate.php'); ?>
