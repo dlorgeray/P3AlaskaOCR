e6103de129a6c79d8b29343e2dff8f50e94fe2c9
dlorgeray <damien.lorgeray@gmail.com>
2018-10-25 18:43:01 +0200

    Modification CSS

--- Content/vendors/echarts/extension/echarts.js
+++
@@ -1,3 +0,0 @@
-define('echarts', [], function () {
-    return echarts;
-});
--- Content/vendors/parsleyjs/src/parsley.js
+++
@@ -1,9 +0,0 @@
-import Parsley from './parsley/main';
-import './parsley/pubsub';
-import './parsley/remote';
-import './i18n/en';
-import inputevent from './vendor/inputevent';
-
-inputevent.install();
-
-export default Parsley;
