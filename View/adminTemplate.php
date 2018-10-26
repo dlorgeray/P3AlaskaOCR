136ef90ff1633720f1c67ae682016f0b5a49ddfc
dlorgeray <damien.lorgeray@gmail.com>
2018-10-26 10:07:32 +0200

    Modification mineure de adminTemplate.php transfert d'une partie du code vers AdminNavTemplate.php

--- View/adminTemplate.php
+++ View/adminTemplate.php
@@ -1,12 +1,8 @@
 <?php include('View/Template/AdminHeaderTemplate.php'); ?>
 
-<body class="nav-md">
-<div class="container body">
-    <div class="main_container">
-        <div class="col-md-3 left_col">
-            <div class="left_col scroll-view">
 
-                <?php include('View/Template/AdminNavTemplate.php'); ?>
+
+<?php include('View/Template/AdminNavTemplate.php'); ?>
                 <!-- content -->
 
                 <?= $content ?>
