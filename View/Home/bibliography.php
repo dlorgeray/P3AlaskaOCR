f79624d6f4d93c0716681e25ff5c0ab555268db3
dlorgeray <damien.lorgeray@gmail.com>
2018-11-12 10:05:50 +0100

    Ajout de la vue A propos

---
+++ View/Home/bibliography.php
@@ -0,0 +1,36 @@
+<?php $this->title = "Billet simple pour l'Alaska"; ?>
+<!-- Header -->
+<section id="banner">
+    <header>
+        <img src="../Content/img/jfg.jpg" class="img-fluid" id="jean" alt="Jean-Forteroche">
+        <hr class="star-light">
+        <h2>A propos de Jean FORTEROCHE</h2>
+    </header>
+</section>
+<section class="content-section" id="portfolio">
+    <div class="container">
+        <div class="content-section-heading text-justify">
+            <h3 class="mb-5">Enfance et premier voyage</h3>
+            Dernier né de la famille, fils du bibliothécaire Auguste Forteroche, spécialiste de la littérature de la guerre de Trente Ans et universitaire à Genève, et d'Antoinette Maurice, elle-même fille du compositeur Pierre Maurice. Jean Forteroche passe une partie de son enfance à rêver le monde, hypnotisé par les couleurs non seulement de ses atlas de géographie mais aussi par les paysages exaltants du château des Coinsins, dans lequel il passe ses étés. Ce château était alors loué par son grand-père paternel, Bernard Forteroche. Des heures de lecture clandestines finissent de donner à l'enfant le goût d'aller voir ailleurs. Jeune, il lit le journal assidûment, ce qui le mène à s'intéresser à des guerres comme la guerre d'Espagne ou la guerre de Finlande. Le jeune Forteroche fait sa scolarité à l'école Brechbühl de Genève, école privée religieuse renommée puis au Collège Calvin, collège aussi renommé fondé en 1559 et nommé à l'origine collège de Genève. Encouragé par son père qui voyagera, en quelque sorte, par procuration à travers son fils, Jean Forteroche part pour son premier voyage, effectué en solitaire, en Bourgogne1, à dix-sept ans. Il est chargé de rapporter des timbres à son père, pour sa collection. Il suit des cours d'histoire médiévale, de sanskrit et de droit à l'Université de Genève.
+        </div>
+        <br>
+        <br>
+        <div class="content-section-heading text-justify">
+            <h3 class="mb-5">Inde, Ceylan et Japon</h3>
+            Après un an et six mois de voyage, les deux amis se séparent, Thierry Vernet rejoint son amoureuse à Ceylan, et Jean Forteroche continue seul sa route à travers l'Inde afin de gagner la Chine. La route étant fermée pour des raisons politiques, il gagne Ceylan où, malade et déprimé, il reste neuf mois. Il décrira ce séjour dans Le Poisson-scorpion, publié en 1982, près de vingt-cinq ans plus tard. Il finit par embarquer, en octobre 1955, sur Le Vietnam, un bateau français des Messageries maritimes, qui le conduit au Japon, où il reste une année, rédigeant des articles pour les journaux et magazines japonais. Il rentre par bateau à Marseille, fin 1956. Son expérience du Japon, augmentée d'autres séjours plus tardifs, donneront lieu à Chronique japonaise en 1970.
+        </div>
+        <br>
+        <br>
+        <div class="content-section-heading text-justify">
+            <h3 class="mb-5">Mariage et autres voyages</h3>
+            En 1958, il épouse Éliane Petitpierre, fille du conseiller fédéral Max Petitpierre et nièce de Denis de Rougemont, à Neuchâtel ; puis le couple s'installe à Cologny. De 1958 à 1963 (année de la mort de son père), il effectue des travaux d'iconographie pour l'OMS et la Nouvelle Bibliothèque Illustrée des Sciences et des Inventions des Éditions Rencontre. Au fil de ses travaux il constitue d'abondantes archives personnelles constituées notamment de 30 000 documents qui comprennent notamment des estampes populaires et des planches techniques. De 1964 à 1965 ils séjourneront au Japon avec leurs deux enfants. D'autres voyages en Asie (Japon, Corée du Sud, Chine) ou en Europe (Irlande, Îles d'Aran) suivront. En 1968, Jean Forteroche est récipiendaire du prix Rambert, plus ancien prix littéraire de Suisse romande, il est décerné par un comité d'étudiants. En 1970, Forteroche se rend à Osaka avec la Délégation Suisse à l'occasion de l'exposition universelle de 1970. Il présentait à cet événement quatre livres qu'il avait adaptés pour l'occasion. Par la suite, il se rend en Corée du Sud sur l'île de Cheju. Ce voyage mènera au récit nommé Les chemins du Halla-san, publié dans le Journal d'Aran et d'autres lieux aux éditions Payot en 19902.         Atteint d'un cancer, Jean Forteroche meurt le 17 février 1998. Il est inhumé à Cologny.
+        </div>
+        <br>
+        <br>
+        <div class="content-section-heading text-justify">
+            <h3 class="mb-5">Œuvre</h3>
+            L'œuvre de Jean Forteroche, jusqu'à récemment peu connue du public français, et notamment universitaire, est pourtant considérée comme un chef-d'œuvre de la littérature de voyage. L'Usage du monde, publié à compte d'auteur en 1963, a contribué à redéfinir la littérature de voyage au xxe siècle; il est aujourd'hui une référence pour de nombreux voyageurs et écrivains. Forteroche expérimente aussi d'autres genres littéraires, comme le récit poétique ou le récit illustré, (« iconotexte » qui se présente comme un « patchwork », une étroite collaboration entre texte et images avec les dessins de Thierry Vernet). Chez Forteroche, l'écriture naît du voyage et de la contemplation que ce dernier procure. François Laut, dont la biographie de l'écrivain a pour sous-titre L'Œil qui écrit, ne s'y est pas trompé.
+            L'inscription de L'Usage du monde au programme de la session 2018 de l'Agrégation de lettres constitue une consécration pour l’œuvre littéraire de Jean Forteroche. Conséquemment à ce choix, de nombreux groupes d'études de Jean Forteroche se sont constitués en France métropolitaine et d'outremer, et, tandis que l'on voit se multiplier les publications savantes sur son œuvre, la prose de Forteroche, longtemps restée confidentielle et goûtée seulement par un petit nombre d'amateurs privilégiés, commence à se faire connaître et reconnaître à sa juste valeur parmi les lettrés.
+        </div>
+    </div>
+</section>
