e3eadcc3fe77a9a22209ee0b8f45c9d2c8df6294
dlorgeray <damien.lorgeray@gmail.com>
2018-11-12 16:33:06 +0100

    Suppresion de fichiers inutiles

--- View/Comments/index.php
+++
@@ -1,8 +0,0 @@
-<?php $this->title = "Billet simple pour l'Alaska - " . $this->clean( $post['title'] ); ?>
-
-    <h1 id="titleReply">Commentaires sur le <?= $this->clean( $post['title'] ) ?></h1>
-</header>
-<?php foreach ($comments as $comments): ?>
-    <p><?= $this->clean( $comments['author'] ) ?> dit :</p>
-    <p><?= $this->clean( $comments['content'] ) ?></p>
-<?php endforeach; ?>
